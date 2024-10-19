e1713c3472825c3e35b24ccb9ce1530fecb8e5a5
Proteus <Proteus>
2024-10-19 11:36:21 -0700

    updated README, added RNG fucntion, created more structs for better structure

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,24 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     defer std.process.argsFree(std.heap.page_allocator, args);
 
     if (args.len < 2) return error.ExpectedArgument;
 
     if (std.mem.eql(u8, args[1], "--version") or std.mem.eql(u8, args[1], "-v")) {
-        try stdout.print("Program version: 0.1.0\n", .{});
+        commands.printVersion();
         return;
     }
 
     if (std.mem.eql(u8, args[1], "--games") or std.mem.eql(u8, args[1], "-g")) {
-        for (games.list) |game| {
-            std.debug.print("{s}\n", .{game});
-        }
+        commands.listGames();
+        return;
+    }
+
+    if (std.mem.eql(u8, args[1], "--exercise") or std.mem.eql(u8, args[1], "-e")) {
+        commands.getRandomExercise();
+        std.debug.print("Random number is: {!}\n", .{tools.randomNumberGenerator()});
         return;
     }
 }
@@ -33,27 +35,60 @@ const Color = struct {
     const RESET = "\x1b[0m";
 };
 
-fn printColor(colorCode: []const u8, inputString: []const u8) void {
-    const stdout = std.io.getStdOut().writer();
-    stdout.print("{s}{s}{s}", .{ colorCode, inputString, Color.RESET }) catch unreachable;
-}
+const tools = struct {
+    fn printColor(colorCode: []const u8, inputString: []const u8) void {
+        const stdout = std.io.getStdOut().writer();
+        stdout.print("{s}{s}{s}", .{ colorCode, inputString, Color.RESET }) catch unreachable;
+    }
 
-fn getUserInput() u8 {
-    const stdin = std.io.getStdIn().reader();
-    const emptyLine = stdin.readUntilDelimiterAlloc(
-        std.heap.page_allocator,
-        '\n',
-        8192,
-    ) catch return 0;
-    defer std.heap.page_allocator.free(emptyLine);
-    const line = std.mem.trim(u8, emptyLine, "\r");
+    fn getUserInput() u8 {
+        const stdin = std.io.getStdIn().reader();
+        const emptyLine = stdin.readUntilDelimiterAlloc(
+            std.heap.page_allocator,
+            '\n',
+            8192,
+        ) catch return 0;
+        defer std.heap.page_allocator.free(emptyLine);
+        const line = std.mem.trim(u8, emptyLine, "\r");
 
-    const usersInput = std.fmt.parseInt(u8, line, 10) catch {
-        printColor(Color.red, "Please enter a valid number.\n");
-        return 0;
-    };
-    return usersInput;
-}
+        const usersInput = std.fmt.parseInt(u8, line, 10) catch {
+            printColor(Color.red, "Please enter a valid number.\n");
+            return 0;
+        };
+        return usersInput;
+    }
+
+    fn randomNumberGenerator() !u8 {
+        var prng = std.rand.DefaultPrng.init(blk: {
+            var seed: u64 = undefined;
+            try std.posix.getrandom(std.mem.asBytes(&seed));
+            break :blk seed;
+        });
+        const rand = prng.random();
+        return rand.int(u8);
+    }
+};
+
+const commands = struct {
+    fn printVersion() void {
+        tools.printColor(Color.YELLOW, "Tonka Version 0.1.0\n");
+    }
+
+    fn listGames() void {
+        for (games.list) |game| {
+            std.debug.print("{s}\n", .{game});
+        }
+    }
+
+    fn getRandomExercise() void {
+        //        const randomExercise: u8 = tools.randomNumberGenerator();
+        //        std.debug.print("{Your exercise is: }\n", .{exercises.list[randomExercise]});
+    }
+};
+
+const exercises = struct {
+    const list = [_][]const u8{ "Pushups", "Pullups", "Situps", "Plank", "Squats", "Curls", "Lunges", "Jumping Jacks" };
+};
 
 const games = struct {
     const list = [_][]const u8{ "val", "valorant", "fn", "fortnite", "mc", "minecraft", "chess" };
@@ -62,52 +97,52 @@ const games = struct {
         // List possible commands
     }
 
-    //Create a function to list games
+    fn coding() void {}
 
     fn valorant() void {
-        const roundsWon: u8 = getUserInput();
-        const roundsLoss: u8 = getUserInput();
-        const kills: u8 = getUserInput();
-        const deaths: u8 = getUserInput();
-        const assists: u8 = getUserInput();
+        const roundsWon: u8 = tools.getUserInput();
+        const roundsLoss: u8 = tools.getUserInput();
+        const kills: u8 = tools.getUserInput();
+        const deaths: u8 = tools.getUserInput();
+        const assists: u8 = tools.getUserInput();
 
         const total = 2 * (5 + (2 * deaths) - (1.5 * kills) - (assists / 1.5) -
             (roundsWon / 2) + roundsLoss / 2);
 
-        printColor(Color.GREEN, "Total Reps: ");
+        tools.printColor(Color.GREEN, "Total Reps: ");
         std.debug.print("{}\n", .{total});
     }
 
     fn minecraft() void {
-        const deaths: u8 = getUserInput();
-        const timePlayedHours: u8 = getUserInput();
-        const levelsXP: u8 = getUserInput();
+        const deaths: u8 = tools.getUserInput();
+        const timePlayedHours: u8 = tools.getUserInput();
+        const levelsXP: u8 = tools.getUserInput();
 
         const total = 10 + (3 * deaths) - (2 * timePlayedHours) - (levelsXP / 2);
 
-        printColor(Color.GREEN, "Total Reps: ");
+        tools.printColor(Color.GREEN, "Total Reps: ");
         std.debug.print("{}\n", .{total});
     }
 
     fn fortnite() void {
-        const elims: u8 = getUserInput();
-        const deathKnock: u8 = getUserInput();
-        const assists: u8 = getUserInput();
+        const elims: u8 = tools.getUserInput();
+        const deathKnock: u8 = tools.getUserInput();
+        const assists: u8 = tools.getUserInput();
 
         const total: i16 = 10 + (3 * deathKnock) - (2 * elims) - (assists / 2);
 
-        printColor(Color.GREEN, "Total Reps: ");
+        tools.printColor(Color.GREEN, "Total Reps: ");
         std.debug.print("{}\n", .{total});
     }
 
     fn chess() void {
-        const piecesTaken: u8 = getUserInput();
-        const piecesStolen: u8 = getUserInput();
-        const winOrLose: u8 = getUserInput();
+        const piecesTaken: u8 = tools.getUserInput();
+        const piecesStolen: u8 = tools.getUserInput();
+        const winOrLose: u8 = tools.getUserInput();
 
         const total: i16 = 10 + (3 * piecesStolen) - (2 * piecesTaken) - (winOrLose / 2);
 
-        printColor(Color.GREEN, "Total Reps: ");
+        tools.printColor(Color.GREEN, "Total Reps: ");
         std.debug.print("{}\n", .{total});
     }
 };
